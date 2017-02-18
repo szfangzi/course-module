41ea75a75c0f35f583e961b97de52ceafd3942e3
fangzi <szfangziwei@qq.com>
2017-02-19 00:43:55 +0800

    yes

--- webpack/vue2/src/main.js
+++ webpack/vue2/src/main.js
@@ -2,21 +2,23 @@ import Vue from 'vue';
 import routes from './routes.js'
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        currentRoute: window.location.hash
-    },
-    created: function() {
-		  window.addEventListener('hashchange', () => {
-  			app.currentRoute = window.location.hash
-		  });
-    },
-    computed: {
-        ViewComponent() {
-            return routes[this.currentRoute]
-        }
-    },
-    render(h){
-        return h(this.ViewComponent)
+  el: '#app',
+  data: {
+    currentRoute: window.location.hash
+  },
+  created: function () {
+    window.addEventListener('hashchange', () => {
+      app.currentRoute = window.location.hash
+    });
+  },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute]
     }
+  },
+  render(h) {
+    return h(this.ViewComponent)
+  }
+
 });
+
